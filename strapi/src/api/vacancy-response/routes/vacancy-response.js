4e2fae271313ef49b661afdb4c54fb387cfa23e0
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2023-01-16 14:23:58 +0400

    setting up basic entities on the backend

---
+++ strapi/src/api/vacancy-response/routes/vacancy-response.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vacancy-response router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::vacancy-response.vacancy-response');
---
+++ strapi/src/api/vacancy-response/services/vacancy-response.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vacancy-response service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::vacancy-response.vacancy-response');
